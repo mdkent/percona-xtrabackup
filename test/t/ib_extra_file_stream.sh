d45846e841e621f93d7955f978a32975af60e8da
Matthew Kent <mkent@magoazul.com>
2013-02-25 15:10:57 -0600

    Add a repeating --extra-file option to allow the inclusion of arbitrary files within the backup.

---
+++ test/t/ib_extra_file_stream.sh
@@ -0,0 +1,26 @@
+########################################################################
+# Test that --extra-file is included in a stream backup
+########################################################################
+
+. inc/common.sh
+
+start_server
+
+rm -rf $topdir/random
+mkdir -p $topdir/random/dir
+echo "123" > $topdir/random/dir/an_extra_file1
+echo "456" > $topdir/random/dir/an_extra_file2
+
+# take a backup with stream mode
+mkdir -p $topdir/backup
+innobackupex --stream=tar \
+  --extra-file=$topdir/random/dir/an_extra_file1 \
+  --extra-file=$topdir/random/dir/an_extra_file2 \
+  $topdir/backup > $topdir/backup/stream.tar
+
+if $TAR itf $topdir/backup/stream.tar | grep ^an_extra_file -c | grep 2; then
+    vlog "--extra-file was backed up"
+else
+    vlog "--extra-file was not backed up"
+    exit -1
+fi
